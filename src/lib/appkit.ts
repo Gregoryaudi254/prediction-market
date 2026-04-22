91c7f476da9ef0fc94325c953155282801913331
Gregoryaudi254 <otile992@gmail.com>
2026-04-22 14:39:06 +0300

    Replace polygonAmoy with polygon in appkit.ts

--- src/lib/appkit.ts
+++ src/lib/appkit.ts
@@ -1,10 +1,10 @@
 import type { AppKitNetwork } from '@reown/appkit/networks'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { polygonAmoy } from '@reown/appkit/networks'
+import { polygon } from '@reown/appkit/networks'
 
 export const projectId = process.env.REOWN_APPKIT_PROJECT_ID ?? ''
 
-export const defaultNetwork = polygonAmoy
+export const defaultNetwork = polygon
 export const networks = [defaultNetwork] as [AppKitNetwork, ...AppKitNetwork[]]
 
 export const wagmiAdapter = new WagmiAdapter({
