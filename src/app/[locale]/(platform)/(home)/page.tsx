adf8d8932afe514710ec65680ef5387aa0a6c1fa
Gregoryaudi254 <otile992@gmail.com>
2026-04-23 00:05:50 +0300

    Refactor HomePage to use cached content

--- src/app/[locale]/(platform)/(home)/page.tsx
+++ src/app/[locale]/(platform)/(home)/page.tsx
@@ -1,10 +1,17 @@
-'use cache'
-
+import type { SupportedLocale } from '@/i18n/locales'
 import { setRequestLocale } from 'next-intl/server'
 import HomeContent from '@/app/[locale]/(platform)/(home)/_components/HomeContent'
 
-export default async function HomePage({ params }: PageProps<'/[locale]'>) {
-  const { locale } = await params
-  setRequestLocale(locale)
+async function CachedHomePageContent({ locale }: { locale: SupportedLocale }) {
+  'use cache'
+
   return <HomeContent locale={locale} />
+}
+
+export default async function HomePage(props: PageProps<'/[locale]'>) {
+  const params = await props.params
+  const { locale } = params
+  setRequestLocale(locale)
+  const resolvedLocale = locale as SupportedLocale
+  return <CachedHomePageContent locale={resolvedLocale} />
 }
