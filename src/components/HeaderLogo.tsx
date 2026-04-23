cb4b58b8f93e2e9f2c7437aca3cbab96e2b99693
Gregoryaudi254 <otile992@gmail.com>
2026-04-24 02:28:37 +0300

    Replace static label with dynamic label prop

--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -29,7 +29,7 @@ export default function HeaderLogo({ labelSuffix }: HeaderLogoProps) {
         imageClassName="size-[1em] object-contain"
         size={32}
       />
-      <span>ChezaCheza</span>
+      <span>{label}</span>
     </AppLink>
   )
 }
